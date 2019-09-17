90d351123121b3a199288a2d253f55afe8e7c0e5
Kelly Meier <kemeier2000@yahoo.com>
2019-09-17 09:15:32 -0500

    added variables to liri.js

--- liri.js
+++ liri.js
@@ -1,5 +1,12 @@
 require("dotenv").config();
 
 var keys = require("./keys.js");
+var moment = require("moment");
+var axios = require("axios");
+var fs = require("fs");
 
+var Spotify = new Spotify require("node-spotify-api");
 var spotify = new Spotify(keys.spotify);
+
+var userRequest = process.argv[2];
+var userSpecific = process.argv[3];
